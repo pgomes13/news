fe88f4dfd6e0d13ad381ba3929d4668fad1eeaad
Pascoal Gomes <pascoal.gomes@localsearch.com.au>
2016-04-18 16:43:42 +1000

    implemented the schema. not working yet

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -9,7 +9,12 @@ function($stateProvider, $urlRouterProvider) {
     .state('home', {
       url: '/home',
       templateUrl: '/home.html',
-      controller: 'MainCtrl'
+      controller: 'MainCtrl',
+      resolve: {
+	    postPromise: ['posts', function(posts){
+	      return posts.getAll();
+	    }]
+  	  }
     })
 
     .state('posts', {
@@ -22,10 +27,17 @@ function($stateProvider, $urlRouterProvider) {
 }]);
 
 
-app.factory('posts', [function(){
+app.factory('posts', ['$http', function(){
   var o = {
     posts: []
   };
+
+  o.getAll = function() {
+    return $http.get('/posts').success(function(data){
+      angular.copy(data, o.posts);
+    });
+  };
+
   return o;
 }]);
 
